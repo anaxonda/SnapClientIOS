5e5d98702438ac2e1fc1aef99e4278440ce3011b
cladonia wainioi <15372684+anaxonda@users.noreply.github.com>
2026-01-18 16:07:55 -0600

    Decode flac per chunk for jitter buffer

--- SnapClientIOS/Session/FlacDecoder.m
+++ SnapClientIOS/Session/FlacDecoder.m
@@ -7,24 +7,19 @@
 
 #import "FlacDecoder.h"
 #include "FLACiOS/stream_decoder.h"
-@import AVFoundation;
-
-#define NUM_BUFFERS 3
-#define BUFFER_SIZE 19200
 
 @interface FlacDecoder () {
     dispatch_queue_t decoderQueue;
-    TPCircularBuffer circularBuffer;
     FLAC__StreamDecoder *decoder;
     StreamInfo *streamInfo;
-    AudioQueueRef audioQueue;
-    AudioQueueBufferRef audioQueueBuffers[NUM_BUFFERS];
 }
 
-@property (atomic) BOOL isDecoding;
 @property (atomic, assign) int32_t currentSec;
 @property (atomic, assign) int32_t currentUsec;
-@property (atomic, assign) NSUInteger dropCount;
+@property (nonatomic, strong) NSData *decodeBuffer;
+@property (nonatomic, assign) NSUInteger decodeOffset;
+@property (nonatomic, strong) NSMutableData *pcmOutput;
+@property (nonatomic, assign) BOOL decoderInitialized;
 
 @end
 
@@ -33,12 +28,11 @@
 - (instancetype)init {
     if (self = [super init]) {
         decoderQueue = dispatch_queue_create("ljk.SnapClientIOS.decoderqueue", NULL);
-        TPCircularBufferInit(&circularBuffer, 262144);
         if ((decoder = FLAC__stream_decoder_new()) == NULL) {
             NSLog(@"Error allocating FLAC decoder!");
             @throw NSInternalInconsistencyException;
         }
-        self.dropCount = 0;
+        self.decoderInitialized = NO;
     }
     return self;
 }
@@ -49,120 +43,118 @@
 
 - (void)setCodecHeader:(NSData *)codecHeader {
     _codecHeader = codecHeader;
-    TPCircularBufferProduceBytes(&circularBuffer, [codecHeader bytes], (uint32_t)codecHeader.length);
-    
-    // attempt to initialize the decoder
-    FLAC__StreamDecoderInitStatus init_status;
-    init_status = FLAC__stream_decoder_init_stream(decoder, read_cb, NULL, NULL, NULL, NULL, write_cb, metadata_cb, error_cb, (__bridge void *)(self));
-    if (init_status != FLAC__STREAM_DECODER_INIT_STATUS_OK) {
-        const char * errorMessage = FLAC__StreamDecoderInitStatusString[init_status];
-        NSLog(@"Error initializing decoder: %@", [[NSString alloc] initWithBytes:errorMessage length:sizeof(errorMessage) encoding:NSASCIIStringEncoding]);
-        @throw NSInternalInconsistencyException;
+
+    if (!self.decoderInitialized) {
+        FLAC__StreamDecoderInitStatus init_status;
+        init_status = FLAC__stream_decoder_init_stream(decoder, read_cb, NULL, NULL, NULL, NULL, write_cb, metadata_cb, error_cb, (__bridge void *)(self));
+        if (init_status != FLAC__STREAM_DECODER_INIT_STATUS_OK) {
+            const char *errorMessage = FLAC__StreamDecoderInitStatusString[init_status];
+            NSString *errorString = [NSString stringWithUTF8String:errorMessage];
+            NSLog(@"Error initializing decoder: %@", errorString ?: @"unknown");
+            @throw NSInternalInconsistencyException;
+        }
+        self.decoderInitialized = YES;
     }
-    
-    // attempt to get the sample rate
-    FLAC__stream_decoder_process_until_end_of_metadata(decoder);
+
+    self.decodeBuffer = codecHeader;
+    self.decodeOffset = 0;
+    if (!FLAC__stream_decoder_process_until_end_of_metadata(decoder)) {
+        NSLog(@"Error processing FLAC metadata");
+    }
+    if (!FLAC__stream_decoder_reset(decoder)) {
+        NSLog(@"Error resetting FLAC decoder after header");
+    }
+    self.decodeBuffer = nil;
 }
 
 - (BOOL)feedAudioData:(NSData *)audioData serverSec:(int32_t)sec serverUsec:(int32_t)usec {
-    if (&streamInfo == NULL) {
+    if (streamInfo == nil) {
         NSLog(@"streamInfo is still NULL, ignoring the audio data for now");
         return NO;
     }
-    
-    if (!TPCircularBufferProduceBytes(&circularBuffer, [audioData bytes], (uint32_t)audioData.length)) {
-        self.dropCount += 1;
-        if (self.dropCount == 1 || (self.dropCount % 50) == 0) {
-            NSLog(@"Decoder buffer full, dropped %lu chunks", (unsigned long)self.dropCount);
-        }
+    if (!self.decoderInitialized || self.codecHeader.length == 0) {
+        NSLog(@"Decoder not initialized or missing codec header");
         return NO;
     }
-    
-    // We update timestamp and trigger decode on the serial queue to ensure correct association
-    // (Assuming one feed = one decode batch roughly, or close enough)
+
+    NSMutableData *buffer = [NSMutableData dataWithCapacity:self.codecHeader.length + audioData.length];
+    [buffer appendData:self.codecHeader];
+    [buffer appendData:audioData];
+
     dispatch_async(decoderQueue, ^{
         self.currentSec = sec;
         self.currentUsec = usec;
-        [self decodeInternal];
-    });
-    
-    return YES;
-}
+        self.decodeBuffer = buffer;
+        self.decodeOffset = 0;
+        self.pcmOutput = [NSMutableData data];
 
-- (void)decodeInternal {
-    self.isDecoding = YES;
-    
-    uint32_t availableBytesFromCircularBuffer;
-    TPCircularBufferTail(&self->circularBuffer, &availableBytesFromCircularBuffer);
-
-    while (availableBytesFromCircularBuffer > 0) {
-        if (!FLAC__stream_decoder_process_single(self->decoder)) {
-            NSLog(@"Error occurred during decoding!");
-            self.isDecoding = NO;
-            return;
+        if (!FLAC__stream_decoder_reset(self->decoder)) {
+            NSLog(@"Error resetting FLAC decoder before chunk");
         }
-        TPCircularBufferTail(&self->circularBuffer, &availableBytesFromCircularBuffer);
-    }
-    
-    self.isDecoding = NO;
+        if (!FLAC__stream_decoder_process_until_end_of_stream(self->decoder)) {
+            NSLog(@"Error occurred during decoding!");
+        }
+
+        NSData *pcmData = [self.pcmOutput copy];
+        self.decodeBuffer = nil;
+        self.pcmOutput = nil;
+
+        if (pcmData.length > 0) {
+            [self.delegate decoder:self didDecodePCMData:pcmData serverSec:sec serverUsec:usec];
+        }
+    });
+
+    return YES;
 }
 
 FLAC__StreamDecoderReadStatus read_cb(const FLAC__StreamDecoder *decoder, FLAC__byte buffer[], size_t *bytes, void *client_data) {
     FlacDecoder *THIS = (__bridge FlacDecoder *)client_data;
-    
-    uint32_t availableBytesFromCircularBuffer;
-    SInt16 *circularBufferData = TPCircularBufferTail(&THIS->circularBuffer, &availableBytesFromCircularBuffer);
-
-    if (availableBytesFromCircularBuffer > 0) {
-        if (availableBytesFromCircularBuffer < *bytes) {
-            // take everything from the circular buffer and fill the decoder's read buffer,
-            // then modify the byte count accordingly
-            memcpy(buffer, circularBufferData, availableBytesFromCircularBuffer);
-            *bytes = availableBytesFromCircularBuffer;
-            TPCircularBufferConsume(&THIS->circularBuffer, availableBytesFromCircularBuffer);
-        } else {
-            // we have more data in the circular buffer than what the decoder needs
-            memcpy(buffer, circularBufferData, *bytes);
-            TPCircularBufferConsume(&THIS->circularBuffer, (uint32_t) *bytes);
-        }
-        
-        return FLAC__STREAM_DECODER_READ_STATUS_CONTINUE;
+    NSData *decodeBuffer = THIS.decodeBuffer;
+    if (!decodeBuffer || THIS.decodeOffset >= decodeBuffer.length) {
+        *bytes = 0;
+        return FLAC__STREAM_DECODER_READ_STATUS_END_OF_STREAM;
     }
-    
-    NSLog(@"Decoder read callback should not be called when there is nothing to decode!");
-    return FLAC__STREAM_DECODER_READ_STATUS_ABORT;
+
+    size_t remaining = decodeBuffer.length - THIS.decodeOffset;
+    size_t toCopy = MIN(remaining, *bytes);
+    memcpy(buffer, ((const uint8_t *)decodeBuffer.bytes) + THIS.decodeOffset, toCopy);
+    THIS.decodeOffset += toCopy;
+    *bytes = toCopy;
+
+    return FLAC__STREAM_DECODER_READ_STATUS_CONTINUE;
 }
 
 FLAC__StreamDecoderWriteStatus write_cb(const FLAC__StreamDecoder *decoder, const FLAC__Frame *frame, const FLAC__int32 * const buffer[], void *client_data) {
     FlacDecoder *THIS = (__bridge FlacDecoder *)client_data;
-    StreamInfo *streamInfo = THIS->streamInfo;
-    
-    // allocate a buffer
-    size_t bytes = frame->header.blocksize * streamInfo.frameSize;
-    int16_t * pcmBuffer = malloc(bytes);
-    
-    for (size_t channel = 0; channel < streamInfo.channels; ++channel) {
-        if (streamInfo.sampleSize == 2) {
-            for (size_t i = 0; i < frame->header.blocksize; i++) {
-                pcmBuffer[streamInfo.channels * i + channel] = (int16_t)buffer[channel][i];
-            }
+    StreamInfo *info = THIS->streamInfo;
+    if (!info || info.sampleSize != 2) {
+        return FLAC__STREAM_DECODER_WRITE_STATUS_CONTINUE;
+    }
+
+    size_t bytes = frame->header.blocksize * info.frameSize;
+    if (!THIS.pcmOutput) {
+        THIS.pcmOutput = [NSMutableData data];
+    }
+    NSUInteger offset = THIS.pcmOutput.length;
+    [THIS.pcmOutput setLength:(offset + bytes)];
+    int16_t *pcmBuffer = (int16_t *)((uint8_t *)THIS.pcmOutput.mutableBytes + offset);
+
+    for (size_t channel = 0; channel < info.channels; ++channel) {
+        for (size_t i = 0; i < frame->header.blocksize; i++) {
+            pcmBuffer[info.channels * i + channel] = (int16_t)buffer[channel][i];
         }
     }
-    
-    NSData *pcmData = [NSData dataWithBytes:pcmBuffer length:(NSUInteger)bytes];
-    free(pcmBuffer);
-    
-    [THIS.delegate decoder:THIS didDecodePCMData:pcmData serverSec:THIS.currentSec serverUsec:THIS.currentUsec];
+
     return FLAC__STREAM_DECODER_WRITE_STATUS_CONTINUE;
 }
 
 void metadata_cb(const FLAC__StreamDecoder *decoder, const FLAC__StreamMetadata *metadata, void *client_data) {
     if (metadata->type == FLAC__METADATA_TYPE_STREAMINFO) {
         FLAC__StreamMetadata_StreamInfo si = metadata->data.stream_info;
-        
+
         StreamInfo *info = [[StreamInfo alloc] initWithSampleRate:si.sample_rate bitsPerSample:si.bits_per_sample channels:si.channels];
         NSLog(@"%@", [info debugDescription]);
-        
+
         FlacDecoder *THIS = (__bridge FlacDecoder *)client_data;
         THIS->streamInfo = info;
     }
